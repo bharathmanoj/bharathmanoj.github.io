3009995c450501dbc5ff324bc61fad2de93f537f
manojbharath26 <manoj.b@volkotech.in>
2022-01-04 08:33:53 +0530

    changes

---
+++ javascript/udemy-js/section2-js-fundamental-part1/vid10-values-variables/vid10-values-variables.js
@@ -0,0 +1,27 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("jonas"); //jonas as a value
+console.log(23); //Here 23 as a value
+
+//variables
+let firstname = "jonas";
+
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
+
+// let jonas&matilda = "JM"; WRONG
+let jonas_matilda = "JM"; //CORRECT
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.245;
+
+let myfirstjob = "Programmer";
+let mycurrectjob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myfirstjob);
---
+++ javascript/udemy-js/section2-js-fundamental-part1/vid9-linking-js-file/vid9-Linking-js-file.js
@@ -0,0 +1,5 @@
+let js = "amazing";
+if (js === "amazing") console.log("Javascript is FUN");
+
+// 40 + 21 - 30;
+console.log(40 + 21 - 30);
