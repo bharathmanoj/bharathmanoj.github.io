86c0f9b186b076250b4ba1a4be512f76d5762889
manojbharath26 <manoj.b@volkotech.in>
2022-02-23 00:05:24 +0530

    changes

--- javascript/50Days-50Projects/proj2-Progress-Steps/script.js
+++ javascript/50Days-50Projects/proj2-Progress-Steps/script.js
@@ -1,50 +1,138 @@
-const progress = document.getElementById("progress");
-const prev = document.getElementById("prev");
-const next = document.getElementById("next");
-const circles = document.querySelectorAll(".circle");
+// const progress = document.getElementById("progress");
+// const prev = document.getElementById("prev");
+// const next = document.getElementById("next");
+// const circles = document.querySelectorAll(".circle");
 
-let currentActive = 1;
+// let currentActive = 1;
 
-next.addEventListener("click", () => {
-  currentActive++;
+// next.addEventListener("click", () => {
+//   currentActive++;
 
-  if (currentActive > circles.length) {
-    currentActive = circles.length;
-  }
+//   if (currentActive > circles.length) {
+//     currentActive = circles.length;
+//   }
 
-  update();
-});
+//   // update();
+//   console.log(update());
+// });
 
-prev.addEventListener("click", () => {
-  currentActive--;
+// prev.addEventListener("click", () => {
+//   currentActive--;
 
-  if (currentActive < 1) {
-    currentActive = 1;
-  }
+//   if (currentActive < 1) {
+//     currentActive = 1;
+//   }
 
-  update();
-});
+//   // update();
+//   console.log(update());
+// });
 
-function update() {
-  circles.forEach((circle, idx) => {
-    if (idx < currentActive) {
-      circle.classList.add("active");
-    } else {
-      circle.classList.remove("active");
-    }
+// function update() {
+//   circles.forEach((circle, idx) => {
+//     if (idx < currentActive) {
+//       circle.classList.add("active");
+//     } else {
+//       circle.classList.remove("active");
+//     }
+//   });
+
+//   const actives = document.querySelectorAll(".active");
+
+//   progress.style.width =
+//     ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+//   if (currentActive === 1) {
+//     prev.disabled = true;
+//   } else if (currentActive === circles.length) {
+//     next.disabled = true;
+//   } else {
+//     prev.disabled = false;
+//     next.disabled = false;
+//   }
+// }
+
+// JQUERY
+
+// $("#progress #prev #next .circle");
+
+// let currentActive = 1;
+
+// $("#next").click(function () {
+//   currentActive++;
+
+//   if (currentActive > circles.length) {
+//     currentActive = circles.length;
+//   }
+
+//   update();
+// });
+
+// $("#prev").click(function () {
+//   currentActive--;
+
+//   if (currentActive < 1) {
+//     currentActive = 1;
+//   }
+
+//   update();
+// });
+
+// $(function update() {
+//   $(".circle").forEach(function (circle, idx) {
+//     if (idx < currentActive) {
+//       circle.classList.add("active");
+//     } else {
+//       circle.classList.remove("active");
+//     }
+//   });
+
+//   $(".active");
+
+//   $("#progress").css(
+//     "width",
+//     "((active.length - 1) / (circle.length - 1)) * 100 + " % ""
+//   );
+
+//   if (currentActive === 1) {
+//     prev.disabled = true;
+//   } else if (currentActive === circle.length) {
+//     next.disabled = true;
+//   } else {
+//     prev.disabled = false;
+//     next.disabled = false;
+//   }
+// });
+
+$("#next").click(function () {
+  $(".circle2").addClass("active");
+  $(".progress").css("width", "33.3333%");
+  $(".btn").removeAttr("disabled");
+
+  $("#next").click(function () {
+    $(".circle3").addClass("active");
+    $(".progress").css("width", "66.6667%");
+
+    $("#next").click(function () {
+      $(".circle4").addClass("active");
+      $(".progress").css("width", "100%");
+      $(".hide").prop("disabled", true);
+    });
   });
+});
 
-  const actives = document.querySelectorAll(".active");
+$("#prev").click(function () {
+  $(".circle4").removeClass("active");
+  $(".progress").css("width", "66.6667%");
+  $(".hide").removeAttr("disabled");
 
-  progress.style.width =
-    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+  $("#prev").click(function () {
+    $(".circle3").removeClass("active");
+    $(".progress").css("width", "33.3333%");
 
-  if (currentActive === 1) {
-    prev.disabled = true;
-  } else if (currentActive === circles.length) {
-    next.disabled = true;
-  } else {
-    prev.disabled = false;
-    next.disabled = false;
-  }
-}
+    $("#prev").click(function () {
+      $(".circle2").removeClass("active");
+      $(".progress").css("width", "0%");
+      $(".hide2").prop("disabled", true);
+    });
+  });
+});
