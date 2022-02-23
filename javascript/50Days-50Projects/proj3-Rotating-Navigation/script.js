b9402c30aa98b3cd3ec483c7349edda71370704b
manojbharath26 <manoj.b@volkotech.in>
2022-02-23 20:39:50 +0530

    changes

--- javascript/50Days-50Projects/proj3-Rotating-Navigation/script.js
+++ javascript/50Days-50Projects/proj3-Rotating-Navigation/script.js
@@ -1,8 +1,18 @@
-const open = document.getElementById("open");
-const close = document.getElementById("close");
-const container = document.querySelector(".container");
+// const open = document.getElementById("open");
+// const close = document.getElementById("close");
+// const container = document.querySelector(".container");
 
-open.addEventListener("click", () => container.classList.add("show-nav"));
+// open.addEventListener("click", () => container.classList.add("show-nav"));
 
-close.addEventListener("click", () => container.classList.remove("show-nav"));
+// close.addEventListener("click", () => container.classList.remove("show-nav"));
 // Explore about the types of function declaration
+
+//ROTATING NAVIGATION JAVASCRIPT TO JQUERY
+
+$("#open").click(function () {
+  $(".container").addClass("show-nav");
+});
+
+$("#close").click(function () {
+  $(".container").removeClass("show-nav");
+});
