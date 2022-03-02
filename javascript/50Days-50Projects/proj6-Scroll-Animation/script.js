a57334090fc4c9e04213fcdcca1fcfef0970cf5b
manojbharath26 <manoj.b@volkotech.in>
2022-03-02 07:07:50 +0530

    changes

--- javascript/50Days-50Projects/proj6-Scroll-Animation/script.js
+++ javascript/50Days-50Projects/proj6-Scroll-Animation/script.js
@@ -1,19 +1,30 @@
-const boxes = document.querySelectorAll(".box");
+// const boxes = document.querySelectorAll(".box");
 
-window.addEventListener("scroll", checkBoxes);
+// window.addEventListener("scroll", checkBoxes);
 
-function checkBoxes() {
-  const triggerBottom = (window.innerHeight / 5) * 4;
+// function checkBoxes() {
+//   const triggerBottom = (window.innerHeight / 5) * 4;
 
-  boxes.forEach((box) => {
-    const boxTop = box.getBoundingClientRect().top;
+//   boxes.forEach((box) => {
+//     const boxTop = box.getBoundingClientRect().top;
 
-    if (boxTop < triggerBottom) {
-      box.classList.add("show");
-    } else {
-      box.classList.remove("show");
-    }
+//     if (boxTop < triggerBottom) {
+//       box.classList.add("show");
+//     } else {
+//       box.classList.remove("show");
+//     }
+//   });
+// }
+
+// checkBoxes();
+
+// Jquery
+
+$(".box1").click(function () {
+  $(".box1").addClass("show");
+  $(".box").getBoundingClientRect().top;
+
+  $(window).click(function () {
+    $(".box").removeClass("show");
   });
-}
-
-checkBoxes();
+});
