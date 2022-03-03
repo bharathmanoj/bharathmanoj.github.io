8688cb0e2110c97c02b298ce608f6277cfa33093
manojbharath26 <manoj.b@volkotech.in>
2022-03-03 22:37:59 +0530

    chanegs

--- javascript/50Days-50Projects/proj7-Split-landing-Page/script.js
+++ javascript/50Days-50Projects/proj7-Split-landing-Page/script.js
@@ -16,38 +16,22 @@
 //   container.classList.remove("hover-right")
 // );
 
-// Jquery
+// JAVASCRIPT CODE TO JQUERY CODE
 
-// $(".split, .left").mouseenter(function () {
-//   $(".container").addClass("hover-left");
-// });
+// Left Side Split Landing Page
+$(".left").mouseenter(function () {
+  $(".container").addClass("hover-left");
 
-// $(".left").mouseenter(function () {
-//   $(".container").removeClass("hover-left");
-// });
+  $(".left").mouseleave(function () {
+    $(".container").removeClass("hover-left");
+  });
+});
 
-// $(".right").mouseenter(function () {
-//   $(".container").addClass("hover-right");
-// });
+// Right Side Split Landing Page
+$(".right").mouseenter(function () {
+  $(".container").addClass("hover-right");
 
-// $(".right").mouseenter(function () {
-//   $(".container").removeClass("hover-right");
-// });
-
-// const left = document.querySelector(".left");
-// const right = document.querySelector(".right");
-// const container = document.querySelector(".container");
-
-// left.addEventListener("mouseenter", () =>
-//   container.classList.add("hover-left")
-// );
-// left.addEventListener("mouseleave", () =>
-//   container.classList.remove("hover-left")
-// );
-
-// right.addEventListener("mouseenter", () =>
-//   container.classList.add("hover-right")
-// );
-// right.addEventListener("mouseleave", () =>
-//   container.classList.remove("hover-right")
-// );
+  $(".right").mouseleave(function () {
+    $(".container").removeClass("hover-right");
+  });
+});
