777008d1cb0683e25c3d7010192036925bcd1913
manojbharath26 <manoj.b@volkotech.in>
2022-03-10 08:12:06 +0530

    changes

--- javascript/50Days-50Projects/proj11-Event-KeyCodes/keycodes.js
+++ javascript/50Days-50Projects/proj11-Event-KeyCodes/keycodes.js
@@ -1,7 +1,28 @@
-const insert = document.getElementById("insert");
+// const insert = document.getElementById("insert");
 
-window.addEventListener("keydown", (event) => {
-  insert.innerHTML = `
+// window.addEventListener("keydown", (event) => {
+//   insert.innerHTML = `
+//   <div class="key">
+//   ${event.key === " " ? "Space" : event.key}
+//   <small>event.key</small>
+// </div>
+
+// <div class="key">
+//   ${event.keyCode}
+//   <small>event.keyCode</small>
+// </div>
+
+// <div class="key">
+//   ${event.code}
+//   <small>event.code</small>
+// </div>
+//   `;
+// });
+
+// jquery
+
+$(window).keydown(function (event) {
+  $("#insert").html(`
   <div class="key">
   ${event.key === " " ? "Space" : event.key} 
   <small>event.key</small>
@@ -15,6 +36,5 @@ window.addEventListener("keydown", (event) => {
 <div class="key">
   ${event.code}
   <small>event.code</small>
-</div>
-  `;
+</div>`);
 });
