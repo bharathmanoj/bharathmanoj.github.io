d0b8ddd74b021873e59ca03c0a80f1b005a75de3
manojbharath26 <manoj.b@volkotech.in>
2022-03-11 08:55:28 +0530

    changes

--- javascript/50Days-50Projects/proj12-FAG-collapse/script.js
+++ javascript/50Days-50Projects/proj12-FAG-collapse/script.js
@@ -1,7 +1,15 @@
-const toggles = document.querySelectorAll(".faq-toggle");
+// const toggles = document.querySelectorAll(".faq-toggle");
 
-toggles.forEach((toggle) => {
-  toggle.addEventListener("click", () => {
-    toggle.parentNode.classList.toggle("active");
+// toggles.forEach((toggle) => {
+//   toggle.addEventListener("click", () => {
+//     toggle.parentNode.classList.toggle("active");
+//   });
+// });
+
+// jquery
+
+$(".faq-toggle").each((toggle) => {
+  $(".faq-toggle").click(toggle, function () {
+    $(this).parent(".faq").toggleClass("active");
   });
 });
