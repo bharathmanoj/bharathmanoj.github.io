61fda2525806436190e59959860b66aef1b6e0c2
manojbharath26 <manoj.b@volkotech.in>
2022-03-16 07:35:25 +0530

    changes

--- javascript/50Days-50Projects/proj15-incrementing-counter/script.js
+++ javascript/50Days-50Projects/proj15-incrementing-counter/script.js
@@ -22,17 +22,27 @@
 
 // Jquery
 
-// const counters = document.querySelectorAll(".counter");
+const $counters = $(".counter");
 
-$(".counter").each((counter) => {
-  // counters.forEach((counter) => {
+// $(".counter").each(function (counter) {
+$counters.each((counter) => {
   counter.innerText = 0;
 
-  const updateCounter = () => {
-    const target = +counter.getAttribute("data-target");
+  function updateCounter() {
+    const target = +counter.attr("data-target");
+    // $(this).attr("data-target") + counter;
+
     const c = +counter.innerText;
 
     const increment = target / 200;
+    // const increment = $(this) / 200;
+
+    // if (c < $(this)) {
+    //   counter.innerText = `${Math.ceil(c + increment)}`;
+    //   setTimeout(updateCounter, 1);
+    // } else {
+    //   counter.innerText = $(this);
+    // }
 
     if (c < target) {
       counter.innerText = `${Math.ceil(c + increment)}`;
@@ -40,7 +50,7 @@ $(".counter").each((counter) => {
     } else {
       counter.innerText = target;
     }
-  };
+  }
 
   updateCounter();
 });
