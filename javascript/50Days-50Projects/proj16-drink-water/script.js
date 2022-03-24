ffe4b789e5fcad94788cc7507eac3208256f7238
manojbharath26 <manoj.b@volkotech.in>
2022-03-24 08:53:55 +0530

    changes

--- javascript/50Days-50Projects/proj16-drink-water/script.js
+++ javascript/50Days-50Projects/proj16-drink-water/script.js
@@ -51,11 +51,67 @@
 
 // // jquery
 
-$(".cup-small").click(function () {
-  $(this).addClass("full");
-  $("#liters").text("1.75L");
-  $("#percentage")
-    .css("visibility", "visible")
-    .css("height", "41.25px")
-    .text("12.5%");
+// $(".cup-small").click(function () {
+//   $(this).addClass("full");
+//   $("#liters").text("1.75L");
+//   $("#percentage")
+//     .css("visibility", "visible")
+//     .css("height", "41.25px")
+//     .text("12.5%");
+
+//   $(".cup-small").click(function () {
+//     $("#liters").text("1.5L");
+//     $("#percentage")
+//       .css("visibility", "visible")
+//       .css("height", "82.2px")
+//       .text("25%");
+//   });
+// });
+
+const smallCups = document.querySelectorAll(".cup-small");
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener("click", () => highlightCups(idx));
 });
+
+function highlightCups(idx) {
+  if (idx === 7 && $(".cup-small")[idx].classList.contains("full")) idx--;
+  else if (
+    $(".cup-small")[idx].classList.contains("full") &&
+    !$(".cup-small")[idx].nextElementSibling.classList.contains("full")
+  ) {
+    idx--;
+  }
+
+  smallCups.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
+      cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
+    }
+  });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = $(".cup-small.full").length;
+
+  const totalCups = $(".cup-small").length;
+
+  if (fullCups === 0) {
+    $("#percentage").css("visibility", "hidden");
+    $("#percentage").css("height", "0");
+  } else {
+    $("#percentage").css("visibility", "visible");
+    $("#percentage").css("height", `${(fullCups / totalCups) * 330}px`);
+    $("#percentage").text(`${(fullCups / totalCups) * 100}%`);
+  }
+
+  if (fullCups === totalCups) {
+    $("#remained").css("visibility", "hidden").css("height", "0");
+  } else {
+    $("#remained").css("visibility", "visible");
+    $("#liters").text(`${2 - (250 * fullCups) / 1000}L`);
+  }
+}
