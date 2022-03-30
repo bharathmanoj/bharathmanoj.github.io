df807c9b1927d97a8ea00a13aae1d4c546c7d8ee
manojbharath26 <manoj.b@volkotech.in>
2022-03-30 08:22:41 +0530

    changes

--- javascript/50Days-50Projects/proj17-movie-app/script.js
+++ javascript/50Days-50Projects/proj17-movie-app/script.js
@@ -1,10 +1,81 @@
+// const API_URL =
+// "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";
+// const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+// const SEARCH_API =
+//   'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
+
+// // console.log(API_URL);
+
+// const main = document.getElementById("main");
+// const form = document.getElementById("form");
+// const search = document.getElementById("search");
+
+// // Get initial movies
+// getMovies(API_URL);
+
+// async function getMovies(url) {
+//   const res = await fetch(url);
+//   const data = await res.json();
+
+//   showMovies(data.results);
+// }
+
+// function showMovies(movies) {
+//   main.innerHTML = "";
+
+//   movies.forEach((movie) => {
+//     const { title, poster_path, vote_average, overview } = movie;
+
+//     const movieEl = document.createElement("div");
+//     movieEl.classList.add("movie");
+
+//     movieEl.innerHTML = `
+//             <img src="${IMG_PATH + poster_path}" alt="${title}">
+//             <div class="movie-info">
+//           <h3>${title}</h3>
+//           <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+//             </div>
+//             <div class="overview">
+//           <h3>Overview</h3>
+//           ${overview}
+//         </div>
+//         `;
+//     main.appendChild(movieEl);
+//   });
+// }
+
+// function getClassByRate(vote) {
+//   if (vote >= 8) {
+//     return "green";
+//   } else if (vote >= 5) {
+//     return "orange";
+//   } else {
+//     return "red";
+//   }
+// }
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   const searchTerm = search.value;
+
+//   if (searchTerm && searchTerm !== "") {
+//     getMovies(SEARCH_API + searchTerm);
+
+//     search.value = "";
+//   } else {
+//     window.location.reload();
+//   }
+// });
+
+// JQUERY
 const API_URL =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
 
-console.log(API_URL);
+// console.log(API_URL);
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
@@ -21,13 +92,14 @@ async function getMovies(url) {
 }
 
 function showMovies(movies) {
-  main.innerHTML = "";
+  $("#main").text("");
 
   movies.forEach((movie) => {
     const { title, poster_path, vote_average, overview } = movie;
 
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie");
+    // $("div").addClass("movie");
 
     movieEl.innerHTML = `
             <img src="${IMG_PATH + poster_path}" alt="${title}">
