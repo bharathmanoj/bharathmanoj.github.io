487cf22141e1dbbbabb2668fdc9276039815ed0e
manojbharath26 <manoj.b@volkotech.in>
2022-04-20 13:49:28 +0530

    webinars-template

--- Html-Css-js-Template/script.js
+++ Html-Css-js-Template/script.js
@@ -5,3 +5,34 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+//
+// const frame = (videos, width) => {
+//   const msg = document.querySelector("#one");
+
+//   msg.innerText = videos;
+//   msg.style.width = width;
+// };
+
+const firstbtn = document.querySelector("#btn1");
+firstbtn.addEventListener("click", function () {
+  // frame("https://www.youtube.com/embed/RbQGn6vBlys");
+  window.location.href = "https://www.youtube.com/embed/RbQGn6vBlys";
+});
+
+const secondbtn = document.querySelector("#btn2");
+secondbtn.addEventListener("click", function () {
+  window.location.href = "https://www.youtube.com/embed/1Rs2ND1ryYc";
+});
+
+const thirdbtn = document.querySelector("#btn3");
+thirdbtn.addEventListener("click", function () {
+  // frame("https://www.youtube.com/embed/W6NZfCO5SIk");
+  window.location.href = "https://www.youtube.com/embed/W6NZfCO5SIk";
+});
+
+const fourthbtn = document.querySelector("#btn4");
+fourthbtn.addEventListener("click", function () {
+  // frame("https://www.youtube.com/embed/JjIvF0yikGU");
+  window.location.href = "https://www.youtube.com/embed/JjIvF0yikGU";
+});
