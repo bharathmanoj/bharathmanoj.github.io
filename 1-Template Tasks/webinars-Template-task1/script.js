0bb9bb9ef428881ccc62646159d5b22afcb90ab7
manojbharath26 <manoj.b@volkotech.in>
2022-04-23 21:46:48 +0530

    changes

---
+++ 1-Template Tasks/webinars-Template-task1/script.js
@@ -0,0 +1,28 @@
+// Mobile Navigation javascript
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// SECTION--1
+$("#btn1").click(function () {
+  $(".iframe1").addClass("show");
+  $(".iframe2, .iframe3, .iframe4").removeClass("show");
+});
+
+$("#btn2").click(function () {
+  $(".iframe2").addClass("show");
+  $(".iframe1, .iframe3, .iframe4").removeClass("show");
+});
+
+$("#btn3").click(function () {
+  $(".iframe3").addClass("show");
+  $(".iframe1, .iframe2, .iframe4").removeClass("show");
+});
+
+$("#btn4").click(function () {
+  $(".iframe4").addClass("show");
+  $(".iframe1, .iframe2, .iframe3").removeClass("show");
+});
