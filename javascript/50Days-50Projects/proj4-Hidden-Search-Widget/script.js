d9ad0f497b6a71933170d65d690118d239bac94a
manojbharath26 <manoj.b@volkotech.in>
2022-04-26 16:14:43 +0530

    changes

--- javascript/50Days-50Projects/proj4-Hidden-Search-Widget/script.js
+++ javascript/50Days-50Projects/proj4-Hidden-Search-Widget/script.js
@@ -18,31 +18,11 @@
 
 //HIDDEN SEARCH WIDGET JAVASCRIPT TO JQUERY
 
-// $("#btn").click(function () {
-//   $(".search").addClass("active");
-//   $("#btn").click(function () {
-//     $(".search").removeClass("active");
-//   });
-// });
-
-$("#btn1").click(function () {
-  $(".iframe1").addClass("show");
-  $(".iframe2, .iframe3, .iframe4").removeClass("show");
-});
-
-$("#btn2").click(function () {
-  $(".iframe2").addClass("show");
-  $(".iframe1, .iframe3, .iframe4").removeClass("show");
-});
-
-$("#btn3").click(function () {
-  $(".iframe3").addClass("show");
-  $(".iframe1, .iframe2, .iframe4").removeClass("show");
-});
-
-$("#btn4").click(function () {
-  $(".iframe4").addClass("show");
-  $(".iframe1, .iframe2, .iframe3").removeClass("show");
+$("#btn").click(function () {
+  $(".search").addClass("active");
+  $("#btn").click(function () {
+    $(".search").removeClass("active");
+  });
 });
 
 //
